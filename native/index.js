f16b3f54dae72fa9f503dbf85a5dfa9207c57a75
UnicodingUnicorn <7555ic@gmail.com>
2018-08-25 14:43:56 +0800

    bleh

--- native/index.js
+++ native/index.js
@@ -111,9 +111,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
       current_alpha = alpha;
       if(active.current >= 0 && active.waypoints != null){
         draw_arrow(average_alpha - absolute_angle, canvas.height / 4);
-        let val = (alpha - absolute_angle - 30) * 50;
-        if(val > 0)
-          navigator.vibrate([val, 1000]);
+        // let val = (alpha - absolute_angle - 30) * 50;
+        // if(val > 0)
+        //   navigator.vibrate([val, 1000]);
       }else{
         draw_arrow(average_alpha, canvas.height / 4);
       }
