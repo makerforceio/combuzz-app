03d0bb0b0b1c687bb06f3b06451730f52dfb9eb2
UnicodingUnicorn <7555ic@gmail.com>
2018-08-25 12:47:29 +0800

    navigator.vibrate, not vibrate

--- native/index.js
+++ native/index.js
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
       const alpha = event.alpha; //Yaw (The one we want)
       if(active.current >= 0 && active.waypoints != null){
         draw_arrow(alpha - absolute_angle, canvas.height / 4);
-        vibrate([200, 100]);
+        navigator.vibrate([(absolute_angle - alpha) * 50, 100]);
       }else{
         draw_arrow(alpha, canvas.height / 4);
       }
